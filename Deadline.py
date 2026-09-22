a9b6a643b5694c29228144699d8d1126bea5ff31
Your Name <Faran17@mail.ru>
2026-09-22 15:07:41 +0300

    создание класса данных

--- Deadline.py
+++ Deadline.py
@@ -1,1 +1,14 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Optional
+
+##cоздание класса данных
+@dataclass
+class Task:
+    name: str
+    task_class: str
+    C: float
+    D: float
+    T: float
 
